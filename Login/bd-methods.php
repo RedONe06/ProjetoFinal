12ce585994f0ec234fa57b7a407a141ec1f7bfeb
RedONe06 <andrielejorasdossantos@gmail.com>
2022-11-21 22:25:38 -0300

    :construction: logica do painel

--- Login/bd-methods.php
+++ Login/bd-methods.php
@@ -1,16 +1,19 @@
 <?php
 //TODO Verificar se o email do usuário já existe no banco antes de continuar o cadastro
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = (isset($_POST["id"]) && $_POST["id"] != null) ? $_POST["id"] : "";
     $nome = (isset($_POST["nome"]) && $_POST["nome"] != null) ? $_POST["nome"] : "";
     $email = (isset($_POST["email"]) && $_POST["email"] != null) ? $_POST["email"] : "";
     $senha = (isset($_POST["senha"]) && $_POST["senha"] != null) ? $_POST["senha"] : "";
     $datanascimento = (isset($_POST["datanascimento"]) && $_POST["datanascimento"] != null) ? $_POST["datanascimento"] : "";
     $ativo = false;
-} else {
+}  else  {
+   
+    $id = (isset($_GET["id"]) && $_GET["id"] != null) ? $_GET["id"] : "";
     $nome = null;
     $email = null;
     $senha = null;
     $datanascimento = null;
     $ativo = false;
 }
-?>
--- Login/painel-adm.php
+++ Login/painel-adm.php
@@ -1,5 +1,6 @@
 <?php
 require_once("init.php");
+require("bd-methods.php");
 // painel do adm com a tabela e logout
 ?>
 <!DOCTYPE html>
@@ -16,30 +17,46 @@ require_once("init.php");
 
 <body>
     <h1>PAINEL DO ADM</h1>
-    <form action="" method="POST">
-    <input type="hidden" name="id" <?php
+    <form action="?act=save" method="POST">
+        <input type="text" name="id" <?php
                 if (isset($id) && $id != null || $id != "") {
                     echo "value=\"{$id}\"";
                 }
                 ?> />
-    <input type="text" placeholder="Nome" name="nome" <?php
+        <input type="text" placeholder="Nome" name="nome" <?php
+       
                if (isset($nome) && $nome != null || $nome != "") {
                    echo "value=\"{$nome}\"";
                }
                ?> />
-    <input type="text" placeholder="Email" name="email" <?php
+               <?php var_dump($nome);?>
+        <input type="text" placeholder="Email" name="email" <?php
                if (isset($email) && $email != null || $email != "") {
                    echo "value=\"{$email}\"";
                }
-               ?> />
-    <input type="text" placeholder="Data de nascimento" name="datanascimento" <?php
+    ?> />
+        <input type="date" placeholder="Data de nascimento" name="datanascimento" <?php
                if (isset($datanascimento) && $datanascimento != null || $datanascimento != "") {
                    echo "value=\"{$datanascimento}\"";
                }
                ?> />
-    <input type="submit" value="salvar" />
-    <input type="reset" value="Novo" />
-    <hr>
+
+        <label for="ativo">Ativo</label>
+        <input type="radio" name="ativo" id="ativo" value="Ativo" <?php
+               if (isset($ativo) && $ativo != null || $ativo != "") {
+                   echo "value=\"{$ativo}\"";
+               }
+               ?>>
+
+        <label for="inativo">Inativo</label>
+        <input type="radio" name="ativo" id="inativo" value="Inativo" <?php
+               if (isset($ativo) && $ativo != null || $ativo != "") {
+                   echo "value=\"{$ativo}\"";
+               }
+               ?>>
+
+        <input type="submit" name="salvar" value="Salvar"/>
+        <hr>
     </form>
     <table width="100%">
         <tr>
@@ -74,10 +91,59 @@ require_once("init.php");
                 ?>
     </table>
 
+    <?php
+    // Bloco if que recupera as informações no formulário, etapa utilizada pelo Update
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "upd" && $id != "") {
+    try {
+        $stmt = db_connect()->prepare("SELECT * FROM usuarios WHERE id = ?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            $rs = $stmt->fetch(PDO::FETCH_OBJ);
+            $id = $rs->id;
+            $nome = $rs->nome;
+            $email = $rs->email;
+            $senha = $rs->senha;
+            $datanascimento = $rs->datanascimento;
+            $ativo = $rs->ativo;
+            echo "<input type='text' placeholder='Nome' name='nome' value='{$nome}'>";
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
 
+if(isset($_POST['salvar'])){
+    try {
+        if ($id != "") {
+            $stmt = db_connect()->prepare("UPDATE usuarios SET nome=?, email=?, datanascimento=?, ativo=? WHERE id = ?");
+            $stmt->bindParam(1, $nome);
+            echo $nome;
+            $stmt->bindParam(2, $email);
+            $stmt->bindParam(3, $datanascimento);
+            $stmt->bindParam(4, $ativo);
+            $stmt->bindParam(5, $id);
 
-
-
+            if ($stmt->execute()) {
+                echo "Dados editados com sucesso!";
+                $id = null;
+                $nome = null;
+                $email = null;
+                $senha = null;
+                $datanascimento = null;
+                $ativo = false;
+            } else {
+                throw new PDOException("Erro: Não foi possível executar a declaração sql");
+            }
+        } else {
+            echo "Id não identificado";
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+?>
 
 
     <a href="form-login.php">Voltar pro formulário de login</a>
