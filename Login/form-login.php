87090a4347c1fa96bbd8f90c37e63c2a39f5be16
RedONe06 <andrielejorasdossantos@gmail.com>
2022-11-25 19:51:58 -0300

    :recycle: login refactoring

--- Login/form-login.php
+++ Login/form-login.php
@@ -1,10 +1,8 @@
 <?php
-require('../init.php');
+require('login.php');
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,31 +12,25 @@ require('../init.php');
     <link rel="stylesheet" href="../style.css">
     <title>Document</title>
 </head>
-
 <header>
-
     <nav class="navbar navbar-expand-lg " id="menu">
         <div class="container-fluid">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="nav justify-content-center">
-
                     <li class="li-logo">
-                    <img id="logo" src="../img/logo.png">
+                        <img id="logo" src="../img/logo.png">
                     </li>
-
-                    <li >
+                    <li>
                         <a class="nav-link" aria-current="page" href="../index.php">Home</a>
                     </li>
-                    <li >
+                    <li>
                         <a class="nav-link" aria-current="page" href="form-cadastro.php">Cadastrar</a>
-                    </li>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 </header>
-
 <body>
     <div class="container-fluid d-flex justify-content-center mt-4">
 
@@ -50,11 +42,11 @@ require('../init.php');
             </div>
 
             <div class="input-group mb-3">
-                <input type="password" name="senha" class="form-control" id="password" aria-label="Amount (to the nearest dollar)">
+                <input type="password" name="senha" class="form-control" id="password"
+                    aria-label="Amount (to the nearest dollar)">
                 <span class="input-group-text" id="togglePassword"><img id="olho" src="../img/eyeF.svg"
                         alt="icone de olho"></span>
             </div>
-
 
             <div class="mb-3 form-check d-flex justify-content-end">
                 <div id="emailHelp" class="form-text"><a href="form-cadastro.php"
@@ -66,83 +58,14 @@ require('../init.php');
         </form>
 
     </div>
-    <script>
-        let btn = document.querySelector('#togglePassword');
-        let olho = document.querySelector('#olho');
-
-        btn.addEventListener('click', function () {
-            let input = document.querySelector('#password');
-            if (input.getAttribute('type') == 'password') {
-                input.setAttribute('type', 'text');
-                olho.setAttribute('src', '../img/eye.svg');
-
-            } else {
-                input.setAttribute('type', 'password');
-                olho.setAttribute('src', '../img/eyeF.svg');
-            }
-        });
-    </script>
-
-
+    <script src="../js/login.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-        crossorigin="anonymous"></script>
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
 </body>
-
 </html>
-
 <?php
 if(isset($_POST["login"])){
-
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
-    
-    if (empty($email) || empty($senha))
-    {
-        echo "Informe email e senha";
-        exit;
-    }
-     
-    $senhaHash = make_hash($senha);
-    
-    try{
-        $PDO = db_connect();
-    } catch (PDOException $erro) {
-        echo "Erro na conexão:" . $erro->getMessage();
-    }
-    
-    $sql = "SELECT id, ativo FROM usuarios WHERE email = :email AND senha = :senha";
-    $stmt = $PDO->prepare($sql);
-     
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':senha', $senhaHash);
-     
-    $stmt->execute();
-     
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-     
-    if (count($users) <= 0)
-    {
-        echo "Email ou senha incorretos";
-        exit;
-    }
-     
-    $user = $users[0];
-    
-    if($user['ativo'] == false){
-        echo "Você ainda não está autorizado pelo ADM. Aguarde a autorização.";
-        exit;
-    }
-    
-    session_start();
-    $_SESSION['logged_in'] = true;
-    $_SESSION['user_id'] = $user['id'];
-    $_SESSION['user_name'] = $user['name'];
-    
-    if($_SESSION['user_id'] == "1"){
-        header('Location: painel-adm.php');
-    } else {
-        header('Location: painel.php');
-    }
+    fazer_login();
 }
 ?>
--- Login/login.php
+++ Login/login.php
@@ -1,54 +1,80 @@
 <?php
-require 'init.php';
+require '../init.php';
  
-$email = isset($_POST['email']) ? $_POST['email'] : '';
-$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
+function fazer_login(){
 
-if (empty($email) || empty($senha))
-{
-    echo "Informe email e senha";
-    exit;
-}
- 
-$senhaHash = make_hash($senha);
+    $dados = iniciar_variaveis();
+    $email = $dados["email"];
+    $senha = $dados["senha"];
+     
+    $senhaHash = make_hash($senha);
+    $PDO = conectar_ao_banco();
 
-try{
-    $PDO = db_connect();
-} catch (PDOException $erro) {
-    echo "Erro na conexão:" . $erro->getMessage();
-}
+    $stmt = fazer_select($PDO, $email, $senhaHash);
 
-$sql = "SELECT id, ativo FROM usuarios WHERE email = :email AND senha = :senha";
-$stmt = $PDO->prepare($sql);
- 
-$stmt->bindParam(':email', $email);
-$stmt->bindParam(':senha', $senhaHash);
- 
-$stmt->execute();
- 
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
- 
-if (count($users) <= 0)
-{
-    echo "Email ou senha incorretos";
-    exit;
-}
- 
-$user = $users[0];
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+    if (count($users) <= 0)
+    {
+        echo "Email ou senha incorretos";
+        exit;
+    }
+     
+    $user = $users[0];
 
-if($user['ativo'] == false){
-    echo "Você ainda não está autorizado pelo ADM. Aguarde a autorização.";
-    exit;
+    validar_adm($user);
+    setar_sessao($user);
 }
 
-session_start();
-$_SESSION['logged_in'] = true;
-$_SESSION['user_id'] = $user['id'];
-$_SESSION['user_name'] = $user['name'];
+function iniciar_variaveis(){
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
+    
+    if (empty($email) || empty($senha))
+    {
+        echo "Informe email e senha";
+        exit;
+    }
+    $usuario = array("senha"=> $senha, "email"=> $email);
+    var_dump($usuario);
+    return $usuario;
+}
 
-if($_SESSION['user_id'] == "1"){
-    header('Location: painel-adm.php');
-} else {
-    header('Location: painel.php');
+function conectar_ao_banco(){
+    try{
+        $PDO = db_connect();
+    } catch (PDOException $erro) {
+        $PDO = null;
+        echo "Erro na conexão:" . $erro->getMessage();
+    }
+    return $PDO;
+}
+
+function fazer_select($PDO, $email, $senhaHash){
+    $sql = "SELECT id, ativo FROM usuarios WHERE email = :email AND senha = :senha";
+    $stmt = $PDO->prepare($sql);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':senha', $senhaHash);
+    $stmt->execute();
+    return $stmt;
+}
+
+function validar_adm($user){
+    if($user['ativo'] == false){
+        echo "Você ainda não está autorizado pelo ADM. Aguarde a autorização.";
+        exit;
+    }
+}
+
+function setar_sessao($user){
+    session_start();
+    $_SESSION['logged_in'] = true;
+    $_SESSION['user_id'] = $user['id'];
+    $_SESSION['user_name'] = $user['name'];
+    
+    if($_SESSION['user_id'] == "1"){
+        header('Location: ../adm/painel-adm.php');
+    } else {
+        header('Location: ../painel.php');
+    }
 }
 ?>
