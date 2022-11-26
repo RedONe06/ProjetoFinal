4149de73ce77a3e848940aee006972a4b67df8fd
RedONe06 <andrielejorasdossantos@gmail.com>
2022-11-25 21:08:34 -0300

    :construction: conferindo cadastro de email

--- cadastro/cadastro-methods.php
+++ cadastro/cadastro-methods.php
@@ -3,6 +3,15 @@ require("../global-methods.php");
 
 function cadastrar($nome, $email, $senha, $senha2, $datanascimento, $ativo){
     try {
+        $PDO = conectar_ao_banco();
+        $users = procurar_no_banco($PDO, $email);
+        if (count($users) > 0)
+        {
+            echo "<div class='alert alert-danger d-flex justify-content-center' width='80px' height='80px' role='alert'>
+                    <p text-align= 'center'>Email já cadastrado.</p>
+                  </div>";
+            exit;
+        }
         if(comparar_senhas($senha, $senha2)){
             $stmt = db_connect()->prepare("INSERT INTO usuarios (nome, email, senha, datanascimento, ativo) VALUES (?, ?, ?, ?, ?)");
             $stmt->bindParam(1, $nome);
@@ -52,5 +61,24 @@ function comparar_senhas($senha, $senha2){
     } else{
         return false;
     }
+}
+
+function procurar_no_banco($PDO, $email){
+    $sql = "SELECT email FROM usuarios WHERE email = :email";
+    $stmt = $PDO->prepare($sql);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+    return $users;
+}
+
+function conectar_ao_banco(){
+    try{
+        $PDO = db_connect();
+    } catch (PDOException $erro) {
+        $PDO = null;
+        echo "Erro na conexão:" . $erro->getMessage();
+    }
+    return $PDO;
 }
 ?>
